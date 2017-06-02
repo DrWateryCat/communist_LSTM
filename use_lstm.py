40fc7eecbbbd094367716052eb76caac2dc812f2
DrWateryCat <drwaterycat@gmail.com>
2017-06-02 18:04:34 -0400

    h

--- use_lstm.py
+++ use_lstm.py
@@ -10,7 +10,7 @@ from keras.utils import np_utils
 
 filename = "communist_manifesto.txt"
 raw_text = ""
-with open(filename, mode='r' encoding='utf8') as file:
+with open(filename, mode='r', encoding='utf8') as file:
     for line in file:
         raw_text += line
 raw_text = raw_text.lower()
@@ -32,7 +32,6 @@ dataY = []
 for i in range(0, n_chars - seq_length, 1):
     seq_in = raw_text[i:i + seq_length]
     seq_out = raw_text[i + seq_length]
-
     dataX.append([char_to_int[char] for char in seq_in])
     dataY.append(char_to_int[seq_out])
 
